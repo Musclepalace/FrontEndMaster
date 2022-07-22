c1ecbcadb5daeaac6e4f15036cd8741e37fdf4db
Amel Sijaric <104675015+Ragnarok28@users.noreply.github.com>
2022-07-22 22:16:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-
-menu.addEventListener('click', mobileMenu);
-
 anime({
-    targets:'.serial',
+    targets:'.navbar-brand',
     scale:[2,1],
     opacity:[0,1],
     easeing: 'easeInOutExpo',
+    delay: 500,
 })
 
 anime({
-    targets:'.vision',
+    targets:'.carousel-inner h5',
     scale:[2,1],
     opacity:[0,1],
     easeing: 'easeInOutExpo',
+    delay: 500,
 })
 
 anime({
-    targets:'.services_title',
+    targets:'.about h1',
     translateX:[120,0],
     opacity:[0,1],
     easeing: 'easeInOutExpo',
@@ -30,32 +23,39 @@ anime({
 })
 
 anime({
-    targets:'.about_title',
-    scale:[2,1],
-    opacity:[0,1],
-    easeing: 'easeInOutExpo',
-})
-
-anime({
-    targets:'.technologies h1',
-    scale:[2,1],
-    opacity:[0,1],
-    easeing: 'easeInOutExpo',
-})
-
-anime({
-    targets:'.info h1',
+    targets:'.services h1',
     translateX:[120,0],
     opacity:[0,1],
     easeing: 'easeInOutExpo',
     delay: 500,
 })
 
-anime({
-    targets:'.info a',
-    translateX:[-120,0],
-    opacity:[0,1],
-    easeing: 'easeInOutExpo',
-    delay: 500,
-})
-
+function validateForm() {
+    let name =  document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    let email =  document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.querySelector('.status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    let subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    let message =  document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.querySelector('.status').innerHTML = "Sending...";
+  }
